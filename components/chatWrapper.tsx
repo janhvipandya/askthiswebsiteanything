9e204cb2f0cf5d42fe669a6c7084e3bcc16333a7
Janhvi Pandya <144421392+jojo2101@users.noreply.github.com>
2024-09-13 18:03:52 +0530

    basic chatbot with ai integration without styling

--- components/chatWrapper.tsx
+++ components/chatWrapper.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useChat } from "ai/react"
+import { Messages } from "./messages"
 
 export const ChatWrapper = ({sessionId}: {sessionId: string}) => {
     const {messages, handleInputChange, handleSubmit, input} = useChat({
@@ -13,14 +14,13 @@ export const ChatWrapper = ({sessionId}: {sessionId: string}) => {
     return (
         <div className= "relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
             <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
-            {JSON.stringify(messages)}
+            <Messages messages={messages}/>
             </div>
 
             <form onSubmit={handleSubmit}>
               <input className=" text-black" value={input} onChange={handleInputChange} type="text" />
-             <button type="submit">send</button>
+             <button className="text-white" type="submit">send</button>
             </form>
-            
         </div>
         
     )
---
+++ components/messages.tsx
@@ -0,0 +1,22 @@
+import { type Message as Tmessage } from "ai/react"
+import { Message } from "./message"
+import { MessageSquare } from "lucide-react"
+
+interface MessagesProps {
+    messages: Tmessage[]
+}
+
+export const Messages = ({messages}: MessagesProps) => {
+ return (
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
+        {messages.length ? messages.map((message, i) => (
+            <Message key={i} content={message.content} isUserMessage={message.role === "user"}/>
+        )) : (<div className="flex-1 flex flex-col items-center justify-center gap-2 ">
+            <MessageSquare className="size-8 text-blue-500" />
+            <h3 className="font-semibold text-xl text-white">You're all Set!</h3>
+            <p className="text-zinc-500 text-sm">Ask your first question to get started.</p>
+        </div>)}
+    </div>
+
+ )
+}
